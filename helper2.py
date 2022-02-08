0fa4a1112616d4915422fb1f3249f64671ae3f30
zorian-f <79806814+zorian-f@users.noreply.github.com>
2022-02-08 17:22:09 +0100

    Update helper2.py

--- helper2.py
+++ helper2.py
@@ -1,18 +1,20 @@
 import glob
 import os
+import pickle
 
 import numpy as np
 from utils import get_dataset
 import tensorflow as tf
 import matplotlib.pyplot as plt
-#%matplotlib inline
 
 paths = glob.glob('data/waymo/training_and_validation/*')
 paths.sort()
 
 cdistribution = []
 #counts[,vehicle, pedestrian, cyclist]
-counts = np.zeros((len(paths),3))
+counts = np.zeros((len(paths), 3))
+#Datadump for offline EDA
+data = []
 
 for i, path in enumerate(paths):
 
@@ -20,19 +22,17 @@ for i, path in enumerate(paths):
     dataset = get_dataset(path)
     dataset = dataset.shuffle(100, reshuffle_each_iteration=True)
     batch = dataset.take(10)
-    
+
     for j, rec in enumerate(batch):
+        data.append(rec)
         classes = rec['groundtruth_classes'].numpy()
         _unique, count = np.unique(classes, return_counts=True)
         for k in range(len(count)):
-            counts[i,k] += count[k]     
-   
-    
-#np.save(counts, test) 
+            counts[i, k] += count[k]     
 
 labels = np.arange(0,len(counts))
 
-width = 0.8       # the width of the bars: can also be len(x) sequence
+width = 0.8
 
 fig, ax = plt.subplots()
 
@@ -46,4 +46,3 @@ ax.set_title('Occurences of Classes')
 ax.legend()
 
 plt.savefig('test.png')
-#plt.show()
