3bbe7897450933695bfeae50fae2f12a3b3f23ad
zorian-f <79806814+zorian-f@users.noreply.github.com>
2022-02-08 20:57:24 +0100

    Update local_processing.py

--- local_processing.py
+++ local_processing.py
@@ -1,5 +1,7 @@
 import pickle
 import numpy as np
+from itertools import groupby
+from operator import itemgetter
 
 from matplotlib.cbook import flatten
 
@@ -11,17 +13,40 @@ def main():
     '''
     
     #Loading the dump
-    path = 'PATH TO DUMP'
+    path = 'C:/Users/QXZ0087/Downloads/datadump'
     data = pickle.load(open(path, "rb"))
-
-    #Check if there is a bbox, not within in the Picture
-    a_key = 'groundtruth_boxes'
-    values_of_key = [a_dict[a_key].numpy() for a_dict in data]
-    values_of_key = list(flatten(values_of_key))
-    #bbox coordinates are expected to be 0.0 => x <= 1.0
-    print('Maximum Value for bbox', np.max(values_of_key))
-    print('Minimum Value for bbox', np.min(values_of_key))
-
     
+    def extract_from_dump(key):
+        values = [a_dict[key].numpy() for a_dict in data]
+        return values
+    
+    #Check if there is a bbox, not within in the Picture
+    bboxvalues = extract_from_dump('groundtruth_boxes')
+    bboxvalues = list(flatten(bboxvalues))
+    #bbox coordinates are expected to be 0.0 => x <= 1.0
+    print('Maximum Value for bbox', np.max(bboxvalues))
+    print('Minimum Value for bbox', np.min(bboxvalues))
+
+    #Calculate the overall percentage of the different classes
+    #This can be taken in to consideration for Split creation,
+    #so we have the same Distribution in every set
+    classesval = extract_from_dump('groundtruth_classes')
+    classesval_flat = list(flatten(classesval))
+    val, count = np.unique(classesval_flat, return_counts=True)
+    percentages = count/count.sum(axis=0) * 100
+    print(list(zip(val,percentages)))
+    
+    #because there are relatively less recordings of cyclists
+    #we calculate how many ther are in which tfRecord
+    cyclists = []
+    for idx, classes in enumerate(classesval):
+        if 4 in classes:
+            _val, count = np.unique(classes, return_counts=True)
+            cyclists.append((count[len(count)-1], data[idx//10]['filename'].numpy()))
+
+    first = itemgetter(1)
+    sums = [(k, sum(item[0] for item in tups_to_sum))
+        for k, tups_to_sum in groupby(sorted(cyclists, key=first), key=first)]
+         
 if __name__ == "__main__":
     main()
