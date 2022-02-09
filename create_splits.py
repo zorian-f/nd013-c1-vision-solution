ab309ad559984e20ab564646a1c72b32877b1914
zorian-f <79806814+zorian-f@users.noreply.github.com>
2022-02-09 15:41:04 +0100

    Update create_splits.py

--- create_splits.py
+++ create_splits.py
@@ -7,27 +7,55 @@ import numpy as np
 
 from utils import get_module_logger
 
-
-def split(source, destination):
+def split(data_dir):
     """
-    Create three splits from the processed records. The files should be moved to new folders in the
+    Create three splits from the processed records. The files should be moved to new folders in the 
     same directory. This folder should be named train, val and test.
 
     args:
-        - source [str]: source data directory, contains the processed tf records
-        - destination [str]: destination data directory, contains 3 sub folders: train / val / test
+        - data_dir [str]: data directory, /home/workspace/data/waymo
     """
-    # TODO: Implement function
+    
+    # TODO: Split the data present in `/home/workspace/data/waymo/training_and_validation` into train and val sets.
+    # You should move the files rather than copy because of space limitations in the workspace.
+
+    train_dir = data_dir+'/train'
+    val_dir = data_dir+'/val'
+    train_and_val_dir = data_dir+'/training_and_validation'
+    
+    if not os.path.exists(train_dir):
+        os.makedirs(train_dir)
+    if not os.path.exists(val_dir):
+        os.makedirs(val_dir)
+
+    files = glob.glob(train_and_val_dir+'/*')
+    
+    #For the first try we split the data naively in 80%-20% train and validation
+    random.shuffle(files)
+    train_size = int(len(files)*0.8)
+    train_files = files[:train_size]
+    eval_files = files[train_size:]
+
+    #delete all exisitng files/splits
+    del_files = glob.glob(train_dir+'/*') + glob.glob(val_dir+'/*')
+    if del_files:
+        for file in del_files:
+            os.remove(file)
 
 
-if __name__ == "__main__":
+    #Create symlinks for the splitdata
+    for files in train_files:
+        os.symlink(files, train_dir+'/'+os.path.basename(files))
+    
+    for files in eval_files:
+        os.symlink(files, val_dir+'/'+os.path.basename(files))
+
+if __name__ == "__main__": 
     parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
-    parser.add_argument('--source', required=True,
-                        help='source data directory')
-    parser.add_argument('--destination', required=True,
-                        help='destination data directory')
+    parser.add_argument('--data_dir', default='/home/workspace/data/waymo',
+                        help='data directory')
     args = parser.parse_args()
 
     logger = get_module_logger(__name__)
     logger.info('Creating splits...')
-    split(args.source, args.destination)
+    split(args.data_dir)
