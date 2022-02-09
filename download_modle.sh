8f800d9b3588ccd87135b5047fce3d645c14beee
zorian-f <79806814+zorian-f@users.noreply.github.com>
2022-02-09 15:59:18 +0100

    Download pretrained modle

---
+++ download_modle.sh
@@ -0,0 +1,1 @@
+wget http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_resnet50_v1_fpn_640x640_coco17_tpu-8.tar.gz && mv ssd_resnet50_v1_fpn_640x640_coco17_tpu-8.tar.gz /home/workspace/experiments/pretrained_model/
