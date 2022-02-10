b6374bd52d41acd2a0e117e052d8478c5f17b0d3
zorian-f <79806814+zorian-f@users.noreply.github.com>
2022-02-10 17:55:14 +0100

    RMS brightness

--- local_processing.py
+++ local_processing.py
@@ -1,9 +1,13 @@
 import pickle
+
 import numpy as np
+import matplotlib.pyplot as plt
+
 from itertools import groupby
 from operator import itemgetter
-
 from matplotlib.cbook import flatten
+from PIL import Image
+from PIL import ImageStat
 
 def main():
     '''
@@ -34,19 +38,49 @@ def main():
     classesval_flat = list(flatten(classesval))
     val, count = np.unique(classesval_flat, return_counts=True)
     percentages = count/count.sum(axis=0) * 100
-    print(list(zip(val,percentages)))
+    print(list(zip(val, percentages, count)))
     
     #because there are relatively less recordings of cyclists
-    #we calculate how many ther are in which tfRecord
+    #we calculate how many there are and in which tfRecord they lay
     cyclists = []
     for idx, classes in enumerate(classesval):
         if 4 in classes:
-            _val, count = np.unique(classes, return_counts=True)
-            cyclists.append((count[len(count)-1], data[idx//10]['filename'].numpy()))
-
+            val, count = np.unique(classes, return_counts=True)
+            cyclists.append((count[np.where(val == 4)][0], 
+                             data[idx//10]['filename'].numpy().decode("utf-8")))
     first = itemgetter(1)
-    sums = [(k, sum(item[0] for item in tups_to_sum))
-        for k, tups_to_sum in groupby(sorted(cyclists, key=first), key=first)]
-         
+    sums = [(k, sum(item[0] for item in tups))
+        for k, tups in groupby(sorted(cyclists, key=first), key=first)]
+    print(sums)
+
+
+    brightness = []
+    for idx, _rec in enumerate(data):
+        im = Image.fromarray(extract_from_dump('image')[idx]).convert('L')
+        stat = ImageStat.Stat(im)
+        brightness.append(stat.rms[0])
+        
+    
+    brightness = np.array(brightness)
+    brightness = brightness.reshape(20,10)
+    stats=[np.mean(brightness, axis=1),np.std(brightness, axis=1)]
+
+    labels = np.arange(0,len(stats[0]))
+    width = 0.8
+    fig, ax = plt.subplots()
+
+    ax.bar(labels, stats[0], yerr=stats[1])
+    ax.set_ylabel('Number of Occurences')
+    ax.set_xlabel('Number for the corresponding tfRecord-file')
+    ax.set_title('Occurences of Classes')
+    ax.legend()
+
+    plt.show()
+    plt.savefig('test5.png')
+            
+        
+        
+    
+    print('f')       
 if __name__ == "__main__":
     main()
